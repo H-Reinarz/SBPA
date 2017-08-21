32bb70a60439aa44bd78a20a5b5164e3e549921a
H-Reinarz <henning-r@hotmail.de>
2017-08-21 23:42:26 +0200

    Initial commit!

---
+++ bow_container.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 21 20:39:13 2017
+
+@author: hre070
+"""
+
+import numpy as np
+
+
+class hist:
+    def __init__(self, *args, **kwargs):
+        
+        #Default
+        #Bins
+        #dict
+        #image
+        
+        
+        
+        pass
+
+    def increment(self, key):
+        pass
+    
+    def __iter__(self):
+        pass
+    
+    def __getitem__(self, key):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
+
+    def __call__(self, mode):
+        pass
+    
+    def __str__(self):
+        pass
+    
+
+test = hist()
