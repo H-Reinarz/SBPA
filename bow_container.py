95ca06816caa5b478d9dac1f31a0eee8ca4e9ff5
H-Reinarz <henning-r@hotmail.de>
2017-08-24 16:13:02 +0200

    Advancement on hist.__init__

--- bow_container.py
+++ bow_container.py
@@ -7,24 +7,80 @@ Created on Mon Aug 21 20:39:13 2017
 """
 
 import numpy as np
+from skimage._shared.utils import assert_nD
+from itertools import repeat
+
+
 
 
 class hist:
     def __init__(self, *args, **kwargs):
-        
-        #Default
-        #Bins
-        #dict
-        #image
-        
-        
-        
-        pass
 
+        #Bins
+        if len(args) == 1 and isinstance(args[0], set):
+            bins = args[0]
+            self.keys = dict(zip(bins, range(len(bins))))
+            
+            if kwargs.__contains__("value"):
+                value = kwargs["value"]
+            else:
+                value = 0
+                        
+            self.container = np.array(list(repeat(value, len(bins))))
+            
+            
+        #dict
+        elif len(args) == 1 and isinstance(args[0], dict):
+            input_dict = args[0]
+            self.keys = dict(zip(input_dict.keys(), range(len(input_dict))))
+            
+            self.container = np.array(list(input_dict.values()))
+            
+
+        #image
+        elif len(args) == 1 and isinstance(args[0], np.ndarray):
+            image = args[0]
+            assert_nD(image, 2)
+            
+            self.keys = None
+            
+            self.container, self.bins  = np.histogram(image, kwargs["bins"])        
+            
+        #Call with wrong arguments
+        else:
+            raise ValueError
+
+
+        #Normalize along with instance creation
+        if kwargs["normalize"]:
+            if not kwargs.__contains__("pixel_count"):
+                raise ValueError
+            else:
+                self.normalize(kwargs["pixel count"])
+        else:
+           self.is_normalized = False
+
+
+    
+    def normalize(self, n):
+        self.norm_container = np.zeros(len(self.container))
+        for ix, e1 in enumerate(np.nditer([self.container])):
+            self.norm_container[ix] = round((e1/n*100, 3))
+            
+        #set falg attribute
+        self.is_normalized = True
+
+    
     def increment(self, key):
         pass
     
-    def __iter__(self):
+    def __iter__(self):       
+        pass
+    
+    def keys(self):
+        pass
+    
+    def values(self):
         pass
     
     def __getitem__(self, key):
@@ -40,4 +96,4 @@ class hist:
         pass
     
 
-test = hist()
+#test = hist()
