7dabf5806917a40132ad6193caa5de39b0b936ee
Omnishambles <Guentheg@students.uni-marburg.de>
2017-08-29 16:08:14 +0200

    updated folder structure and setup py

--- setup.py
+++ setup.py
@@ -10,19 +10,22 @@ base_path = os.path.abspath(os.path.dirname(__file__))
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
 
-    config = Configuration('LBP_BINS', parent_package, top_path)
+    config = Configuration('_LBP', parent_package, top_path)
     #config.add_data_dir('tests')
 
-    cython(['lbp_bins.pyx'], working_path=base_path)
+    cython(['src/lbp_bins.pyx'], working_path=base_path)
+    cython(['src/_lbp.pyx'], working_path=base_path)
 
-    config.add_extension('lbp_bins', sources=['lbp_bins.c'],
+    config.add_extension('lbp_bins', sources=['src/lbp_bins.c'],
+                         include_dirs=[get_numpy_include_dirs()])
+    config.add_extension('_lbp', sources=['src/_lbp.c'],
                          include_dirs=[get_numpy_include_dirs()])
 
     return config
 
 if __name__ == '__main__':
     from numpy.distutils.core import setup
-    setup(maintainer='Henning Reinarz',
-          description='LBP_BINS',
+    setup(maintainer='Henning Reinarz and Jannik Guenther',
+          description='LBP_BINS AND LBP',
           **(configuration(top_path='').todict())
           )
