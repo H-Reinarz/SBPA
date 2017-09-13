4a3cc4ac96992bc4e9d426fc37af30572e4c21af
H-Reinarz <henning-r@hotmail.de>
2017-09-13 18:42:12 +0200

    dft summary and derivation plotting

--- frequency.py
+++ frequency.py
@@ -8,14 +8,14 @@ Created on Wed Sep 13 12:22:51 2017
 
 import numpy as np
 from scipy.spatial.distance import euclidean
-#from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 
 
-
+dists = []
 
 def dft_summary(freq, width=1):
         
-    class_array =  np.zeros(shape=freq.shape, dtype=np.int8)
+    class_array =  np.zeros(shape=freq.shape, dtype=np.int32)
     
     middle = (class_array.shape[0]/2, class_array.shape[1]/2)
     
@@ -24,12 +24,13 @@ def dft_summary(freq, width=1):
             
             distance = euclidean((row+1, col+1), middle)
             #print(distance)
+            dists.append(distance)
             
             class_array[row, col] = distance//width
             
             
             
-    #plt.imshow(class_array)
+    plt.imshow(class_array)
     
     class_dict = dict()
     
@@ -41,17 +42,63 @@ def dft_summary(freq, width=1):
     return class_dict
 
 
+#======================================================================================
+from skimage import io
+from skimage.color import rgb2gray
+from skimage.filters import gaussian
+from numpy.fft import *
+
+image = io.imread("/home/hre070/MA/DJI_0095_CLIP.jpg")
+im_gray = rgb2gray(image)
+
+start = 50
+width = 400
+
+
+end  = start+width
+
+clip = im_gray[start:end, start:end]
+
+
+#clip = im_gray
+
+clip = gaussian(clip, sigma=20)
+
+
+#Apply shifted furier transform
+dft = fftshift(fft2(clip))
+
+dft_log = np.log(np.abs(dft))
 
 
 
-#in_shape = (100, 200)
-#width = 1
+
+
+
+
+#in_shape = (10, 20)
+#ring_width = 1
 #
 #dft = np.random.randint(60, 9999, size=in_shape)/10000
-#
-#
-#plot_values = list(dft_summary(dft).values())
-#
-#plt.plot(plot_values)
-#    
-#print(plot_values)
+
+result = dft_summary(dft_log, 10)
+
+plot_values = np.array(list(result.values()))
+
+deriv1 = np.diff(plot_values)
+deriv2 = np.diff(deriv1)
+
+
+f, ax = plt.subplots(nrows=2, figsize=(15,15))
+
+ax[0].imshow(clip, cmap="gray")
+ax[1].imshow(dft_log)
+
+
+f, ax = plt.subplots(nrows=3, figsize=(10,10), sharex=True)
+
+ax[0].plot(plot_values)
+ax[1].plot(deriv1, color="r")
+ax[2].plot(deriv2, color="g")
+    
+print(result)
