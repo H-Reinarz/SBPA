042ac755d7adf81e17ac066b68b2f790439c16b3
j-guenther <jannik.guentheg@gmail.com>
2017-10-06 18:52:43 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  6 18:40:16 2017
+
+@author: Jannik
+"""
+
+import configparser
+
+class dictobject(object):
+    def __init__(self, d):
+        self.__dict__ = d
+
+def GetParams(ini):
+    # https://wiki.python.org/moin/ConfigParserExamples
+    config = configparser.ConfigParser()
+    config.read(ini)
+    config.sections()
+    
+    paramsDict = {}
+    for section in config.sections():
+        dict1 = {}
+        options = config.options(section)
+        for option in options:
+            try:
+                dict1[option] = config.get(section, option)
+                if dict1[option] == -1:
+                    print("skip: %s" % option)
+            except:
+                print("exception on %s!" % option)
+                dict1[option] = None
+            
+        paramsDict[section] = dictobject(dict1)
+        
+    Params = type("Params", (), paramsDict)
+    p = Params()
+    return p
