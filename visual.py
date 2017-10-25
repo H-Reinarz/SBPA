aeafc3216ebc3b963537e928833b0c12c63bf041
H-Reinarz <henning-r@hotmail.de>
2017-10-25 13:01:09 +0200

    Added plot_node_attribute() and bug fix in show_seeds()!

--- visual.py
+++ visual.py
@@ -40,6 +40,41 @@ def plot_sp_labels(axes, labels, fontsize, subset=None, **text_kwargs):
 
 
 
+def plot_node_attribute(axes, graph, attribute, fontsize, subset=None, **text_kwargs):
+    '''Function that plots a specified attribute of a node/region to its centroid.
+    Axes with appropriate background image serve as an argumunt, along with the
+    graph and text parameters.'''
+
+    #increment to include 0
+    graph.seg_img += 1
+
+    #convert fontsize to pt
+    wext = axes.get_window_extent()
+    pt_ratio = wext.height/graph.seg_img.shape[0]
+    pt_size = round(fontsize*pt_ratio)
+
+
+    if subset is not None:
+        subset = set(subset)
+
+    #iteratively plot the labels
+    for reg in regionprops(graph.seg_img):
+        true_label = reg.label-1
+        
+        to_plot = graph.node[true_label][attribute]
+
+        if subset is None or to_plot in subset:
+            y_pos, x_pos = reg.centroid
+            axes.text(x_pos, y_pos, to_plot,
+                      fontsize=pt_size, ha='center', va='center', **text_kwargs)
+
+    #decrement to restore original data
+    graph.seg_img -= 1
+
+
+
+
+
 def show_seeds(axes, labels, seeds, marker, **plot_kwargs):
     '''Function that plots a marker to the centroid of a region (representing a seed).
     Axes with appropriate background image serve as an argumunt, along with
@@ -50,7 +85,7 @@ def show_seeds(axes, labels, seeds, marker, **plot_kwargs):
 
     seeds = set(seeds)
 
-    regs = [reg for reg in regionprops(labels) if reg.label in seeds]
+    regs = [reg for reg in regionprops(labels) if (reg.label - 1) in seeds]
 
     y_list = [reg.centroid[0] for reg in regs]
     x_list = [reg.centroid[1] for reg in regs]
