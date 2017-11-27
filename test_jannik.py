88e7be6f00f48787e9a0f0628ec07481994ee966
j-guenther <jannik.guentheg@gmail.com>
2017-11-27 13:14:34 +0100

    test agglomerative clustering

--- test_jannik.py
+++ test_jannik.py
@@ -15,100 +15,86 @@ import rgb_indices as rgb
 import utils as u
 import numpy as np
 from skimage.util import img_as_float
-
+from skimage.segmentation import slic
+from skimage.segmentation import quickshift
+from skimage.segmentation import mark_boundaries
 
 from skimage.color import rgb2gray, rgb2hsv, hsv2rgb
 from matplotlib import pyplot as plt
 
-import pandas as pd
+import lbp
+from skimage.transform import rotate
+from skimage.color import label2rgb
+
+
 
 image = io.imread("D:/janni/Documents/Geographie/Masterarbeit/Data/ra_neu/ra2_small.jpg")
-#image = io.imread("D:/janni/Documents/Geographie/Masterarbeit/Data/ResearchArea/RA1/orthoRA.jpg")
 
-#image = image.astype('int32')
-#image = img_as_float(image)
-
-#image = u.AddValue(image, 1)
-image = u.ZerosToOne(image, 1)
-image = img_as_float(image)
-
-gli = rgb.GLI(image)
-vvi = rgb.VVI(image)
-ntdi = rgb.NDTI(image)
-ci = rgb.CI(image)
-bi = rgb.BI(image)
-si = rgb.SI(image)
-tgi = rgb.TGI(image)
-ngrdi = rgb.NGRDI(image)
-
-gli = u.NormalizeImage(gli)
-vvi = u.NormalizeImage(vvi)
-ntdi = u.NormalizeImage(ntdi)
-ci = u.NormalizeImage(ci)
-bi = u.NormalizeImage(bi)
-si = u.NormalizeImage(si)
-tgi = u.NormalizeImage(tgi)
-ngrdi = u.NormalizeImage(ngrdi)
-
-df = pd.DataFrame({'gli':gli.flatten(), 'vvi':vvi.flatten(), 'ntdi':ntdi.flatten(), 
-                   'ci':ci.flatten(), 'bi':bi.flatten(), 'si':si.flatten(), 
-                   'tgi':tgi.flatten(), 'ngrdi':ngrdi.flatten(),
-                   'r':image[:,:,0].flatten(), 'g':image[:,:,1].flatten(), 'b':image[:,:,2].flatten()})
-
-
-
-from sklearn.decomposition import PCA
-pca = PCA(n_components=3).fit(df)
-pca_reduced = pca.transform(df)
-
-dim1 = u.ImageFromArray(pca_reduced[:,0], image)
-dim2 = u.ImageFromArray(pca_reduced[:,1], image)
-dim3 = u.ImageFromArray(pca_reduced[:,2], image)
-
-#stack = u.MergeChannels([dim1,dim2,dim3])
-
-#stack = u.NormalizeImage(stack)
-dim1 = u.NormalizeImage(dim1)
-#dim2 = u.NormalizeImage(dim2)
-#dim3 = u.NormalizeImage(dim3)
-#
-#f, ax = plt.subplots(nrows= 4, figsize=(25, 50))
-#ax[0].imshow(stack, cmap='gray')
-#ax[1].imshow(dim1, cmap='gray')
-#ax[2].imshow(dim2, cmap='gray')
-#ax[3].imshow(dim3, cmap='gray')
-
-
-
-
-imageHsv = rgb2hsv(image)
-dim1Inverted = 1-dim1
-imageHsv[:,:,2] = dim1Inverted
-imageHsv = hsv2rgb(imageHsv)
-
-f, ax = plt.subplots(figsize=(17, 17))
-ax.imshow(imageHsv)
-
-#reduced_data = pd.DataFrame(pca_reduced, columns = ['Dimension 1', 'Dimension 2'])
-
-from skimage.segmentation import slic
-from skimage.segmentation import mark_boundaries
-segments_slic = slic(imageHsv, n_segments=700, compactness=20, sigma=1)
+segments_slic = slic(image, n_segments=400, compactness=15, sigma=1)
 print('SLIC number of segments: {}'.format(len(np.unique(segments_slic))))
+f, ax = plt.subplots(figsize=(10, 10))
 
-f, ax = plt.subplots(figsize=(17, 17))
-ax.imshow(mark_boundaries(imageHsv, segments_slic), cmap='gray');
+ax.imshow(mark_boundaries(image, segments_slic))
 
-#stack2 = u.MergeChannels([vvi,bi,tgi])
+im_gray = rgb2gray(image)
+# settings for LBP
+METHOD = 'default'
+radius = 8
+
+n_points = 4 
+
+lbp_img = lbp.local_binary_pattern(im_gray, None, 8, radius, method='default', nilbp = True)
+
+######################################
+
+import networkx as nx
+
+#from bowrag import BOW_RAG, cumu_diff
+import bow_rag
+import bow_diff
+Graph = bow_rag.BOW_RAG(segments_slic)
+print(nx.info(Graph))
+Graph.add_attribute('color', image, np.mean)
+Graph.normalize_attribute('color', value=255)
+Graph.add_attribute('var', im_gray, np.var)
+
+fs_attrs = {'color':1, 'var':0.8}
+fs1 = Graph.get_feature_space_array(fs_attrs)
+
+from sklearn.feature_extraction.image import grid_to_graph
+from sklearn.cluster import AgglomerativeClustering
+import scipy as sp
+
+connectivity = nx.adjacency_matrix(Graph, weight=None)
+
+n_clusters = 2  # number of regions
+
+Graph.clustering('cluster1', 'AgglomerativeClustering', fs1, n_clusters=2, linkage="ward", connectivity=connectivity)
+fs2 = Graph.attribute_divided_fs_arrays(fs_attrs, 'cluster1')
+Graph.clustering('cluster2', 'AgglomerativeClustering', fs2, n_clusters=2, linkage="ward", connectivity=connectivity)
+#ward = AgglomerativeClustering(n_clusters=n_clusters, linkage='ward',
+#                               connectivity=connectivity)
+cluster_img = Graph.produce_cluster_image('cluster2')
+
+from skimage import color
+
+out = color.label2rgb(cluster_img, image, kind='avg')
+out = segmentation.mark_boundaries(out, cluster_img, (0, 0, 0))
+
+from visual import plot_node_attribute
+f, ax = plt.subplots(nrows=2, figsize=(18, 18))
+
+#ax[0].imshow(image)
+ax[0].imshow(im_gray)
+ax[0].imshow(out, alpha = .4)
+ax[0].imshow(cluster_img, cmap=plt.cm.spectral, alpha=.2)
+plot_node_attribute(ax[0], Graph, 'cluster2', 24)
 
 
-"""
+ax[1].imshow(image)
+#ax[1].imshow(redu_out)
+#ax[1].imshow(redu, cmap=plt.cm.spectral, alpha=.2)
+#plot_node_attribute(ax[1], Graph, 'redu1', 12)
+f, ax = plt.subplots(figsize=(15, 15))
 
-
-
-segments_slic = slic(stack2, n_segments=100, compactness=15, sigma=1)
-print('SLIC number of segments: {}'.format(len(np.unique(segments_slic))))
-
-f, ax = plt.subplots(figsize=(17, 17))
-ax.imshow(mark_boundaries(image, segments_slic));
-"""
+ax.imshow(mark_boundaries(image, cluster_img));
