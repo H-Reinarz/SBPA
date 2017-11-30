b7275843f48289500e01791036d6671da493e2e2
j-guenther <jannik.guentheg@gmail.com>
2017-11-30 16:20:07 +0100

    initial commit

---
+++ agglomerativ_clustering.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 30 11:14:34 2017
+
+@author: Jannik
+"""
+import numpy as np
+import networkx as nx
+import bow_rag
+from sklearn.cluster import AgglomerativeClustering
+
+
+def AgglCluster(g, attr_name, fs_spec, n_clusters=2, pixel_min=-1, superpixel_min=2, ):
+    if isinstance(fs_spec, bow_rag.BOW_RAG.fs_spec):
+        connectivity = nx.adjacency_matrix(g, weight=None)
+        g.clustering(attr_name, 'AgglomerativeClustering', fs_spec, n_clusters=n_clusters, linkage="ward", connectivity=connectivity)
+    
+    elif isinstance(fs_spec, list):
+        for node in g.__iter__():
+                g.node[node][attr_name] = None
+        for fs in fs_spec:
+            if not isinstance(fs, bow_rag.BOW_RAG.fs_spec):
+                raise TypeError("Must be BOW_RAG.fs_spec!")
+            
+            if len(fs[1]) < superpixel_min:
+                #print("Cluster too small")
+                g.node[fs[1][0]][attr_name] = str(fs.label)# + str(3)
+                continue
+            
+            subset = g.subgraph(nbunch=list(fs[1]))
+            
+            pixel_count = 0
+            
+            if pixel_min >= 0:
+                for n in subset:
+                    pixel_count += subset.node[n]['pixel_count']
+                if pixel_count <= pixel_min:
+                    #print("In FS ", fs.label, " are ", pixel_count, " Pixel. It gets Name ", fs.label)
+                    for n in subset:
+                        g.node[n][attr_name] = str(fs.label)# + str(4)
+                    continue
+            
+            connectivity = nx.adjacency_matrix(subset, weight=None)
+                        
+            cluster_obj = AgglomerativeClustering(n_clusters=n_clusters, linkage='ward',
+                               connectivity=connectivity).fit(fs.array)       
+            for node, label in zip(fs.order, cluster_obj.labels_):
+                g.node[node][attr_name] = str(fs.label) + str(label)
+
+
+def AgglCluster_Cascade(g, fs_attr, attr_name, n_cascade=2, automatic=False, pixel_min=-1):
+    runs = 0
+    cascade = True
+    fs1 = g.basic_feature_space_array(fs_attr)
+    
+    if not automatic:
+        for run in range(0, n_cascade):
+            AgglCluster(g, attr_name+str(run), fs1, 2, pixel_min)        
+            fs1 = g.attribute_divided_fs_arrays(fs_attr, attr_name+str(run))
+        runs = n_cascade
+    else:
+        while cascade:            
+            AgglCluster(g, attr_name+str(runs), fs1, 2, pixel_min)        
+            fs1 = g.attribute_divided_fs_arrays(fs_attr, attr_name+str(runs))
+            if runs > 0:
+                for n in g:
+                    if g.node[n][attr_name+str(runs-1)] != g.node[n][attr_name+str(runs)]:
+                        cascade = True
+                        break
+                    else:
+                        cascade = False
+            runs += 1
+    return runs
